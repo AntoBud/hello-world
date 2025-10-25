3ba7b4d590f68fd505d8d3f2830cb31ec4056b93
Anto Budiardjo <anto@pointview.com>
2025-10-25 16:58:10 -0400

    Modify hello.js to run on load

--- hello.js
+++ hello.js
@@ -1,5 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const message = document.createElement('p');
-  message.textContent = 'Hello World';
-  document.body.appendChild(message);
-});
+const message = document.createElement('p');
+message.textContent = 'Hello World';
+document.body.appendChild(message);
