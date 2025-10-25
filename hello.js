16e5bb23a646ba4c0ea2815454f7c8024c06ce4e
Anto Budiardjo <anto@pointview.com>
2025-10-25 16:46:20 -0400

    Add hello.js to display Hello World

---
+++ hello.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const message = document.createElement('p');
+  message.textContent = 'Hello World';
+  document.body.appendChild(message);
+});
